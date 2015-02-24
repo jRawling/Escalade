b63a39e443f97dd0c5d3fd0fd0eb17657dee1e1b
Jonathan Rawling <jonathan@rawling.me>
2015-02-24 22:41:19 +0000

    Missed files from previous commit, mostly project files.

---
+++ src/Web.Public/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+
+namespace Escalade.Web.Public.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        { }
+
+        [Route("register")]
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            throw new NotSupportedException();
+        }
+
+        [Route("register")]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Register(object viewModel)
+        {
+            throw new NotSupportedException();
+        }
+
+        [Route("login")]
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            throw new NotSupportedException();
+        }
+
+        [Route("login")]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(object viewModel)
+        {
+            throw new NotSupportedException();
+        }
+
+        [Route("login/facebook")]
+        public IActionResult LoginWithFacebook()
+        {
+            throw new NotSupportedException();
+        }
+
+        [Route("logout")]
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
