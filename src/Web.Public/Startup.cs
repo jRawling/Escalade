9d4d59f5742b00d5ad57c8e8b239d27850e142be
Jonathan Rawling <jonathan@rawling.me>
2015-02-24 22:35:36 +0000

    Added outline for first actions, pages and basic routing.

--- src/Web.Public/Startup.cs
+++ src/Web.Public/Startup.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Routing;
 
 namespace Escalade.Web.Public
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
         }
     }
 }
